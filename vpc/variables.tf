f3c5418f8c2a1ddd28acb05f8a3196111e914f78
lafuan <lafuan@hotmail.com>
2023-01-25 15:24:45 +0700

    Create variables.tf

---
+++ vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "us-west-2"
+  type = string
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+  type = string
+}
+
+variable "subnet_cidr" {
+  default = "10.0.1.0/24"
+  type = string
+}
+
+variable "availability_zone" {
+  default = "us-west-2a"
+  type = string
+}
