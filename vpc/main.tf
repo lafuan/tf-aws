651785bf9b1c9a50942537edb63005fc8d7aaa93
lafuan <lafuan@hotmail.com>
2023-01-25 15:24:18 +0700

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,15 +1,16 @@
 provider "aws" {
-  region = "us-west-2"
+  region = var.region
 }
 
 resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
 }
+
 
 resource "aws_subnet" "example" {
   vpc_id            = aws_vpc.example.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
+  cidr_block        = var.subnet_cidr
+  availability_zone = var.availability_zone
 }
 
 resource "aws_internet_gateway" "example" {
@@ -23,9 +24,4 @@ resource "aws_route_table" "example" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.example.id
   }
-}
-
-resource "aws_route_table_association" "example" {
-  subnet_id      = aws_subnet.example.id
-  route_table_id = aws_route_table.example.id
 }
