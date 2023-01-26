2a0803732ca8b1741dc3124945eb62bfc6deb785
mhmdnaufal <lafuan@hotmail.com>
2023-01-26 13:50:28 +0700

    add vpc main

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,31 +1,60 @@
 provider "aws" {
-  region = var.region
+  region = "${var.region}"
 }
 
-resource "aws_vpc" "example" {
-  cidr_block = var.vpc_cidr
-  
-  tags {
-    Name = "VPC Main"
+resource "aws_vpc" "main" {
+  cidr_block = "${var.vpc_cidr}"
+
+  tags = {
+    Name = "Main VPC"
   }
 }
 
+resource "aws_subnet" "public_subnets" {
+ count      = "${length(var.public_subnet_cidrs)}"
+ vpc_id     = "${aws_vpc.main.id}"
+ cidr_block = "${element(var.public_subnet_cidrs, count.index)}"
+ availability_zone = "${element(var.azs, count.index)}"
 
-resource "aws_subnet" "example" {
-  vpc_id            = aws_vpc.example.id
-  cidr_block        = var.subnet_cidr
-  availability_zone = var.availability_zone
+ tags = {
+   Name = "Public Subnet ${count.index + 1}"
+ }
 }
 
-resource "aws_internet_gateway" "example" {
-  vpc_id = aws_vpc.example.id
+resource "aws_subnet" "private_subnets" {
+ count      = "${length(var.private_subnet_cidrs)}"
+ vpc_id     = "${aws_vpc.main.id}"
+ cidr_block = "${element(var.private_subnet_cidrs, count.index)}"
+ availability_zone = "${element(var.azs, count.index)}"
+
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+ }
 }
 
-resource "aws_route_table" "example" {
-  vpc_id = aws_vpc.example.id
+resource "aws_internet_gateway" "main" {
+  vpc_id = "${aws_vpc.main.id}"
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.example.id
+  tags = {
+    Name = "Main VPC IG"
   }
+}
+
+resource "aws_route_table" "second_rt" {
+ vpc_id = aws_vpc.main.id
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = "${aws_internet_gateway.main.id}"
+ }
+
+ tags = {
+   Name = "2nd Route Table"
+ }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+ count = "${length(var.public_subnet_cidrs)}"
+ subnet_id      = "${element(aws_subnet.public_subnets[*].id, count.index)}"
+ route_table_id = "${aws_route_table.second_rt.id}"
+
 }
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,15 +1,25 @@
 variable "region" {
-  default = "us-west-2"
+  default = "ap-southeast-3"
 }
 
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-variable "subnet_cidr" {
-  default = "10.0.1.0/24"
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public Subnet CIDR values"
+ default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
-variable "availability_zone" {
-  default = "us-west-2a"
+variable "private_subnet_cidrs" {
+ type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "azs" {
+ type        = list(string)
+ description = "Availability Zones"
+ default     = ["ap-southeast-3a", "ap-southeast-3b", "ap-southeast-3c"]
 }
