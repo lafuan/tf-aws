a59c1593120c9bda33d394b9b6df1be157c90350
mhmdnaufal <lafuan@hotmail.com>
2023-01-27 10:20:24 +0700

    add rds

---
+++ rds_instance/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region     = "ap-southeast-3"
+
+}
+
+
+resource "aws_db_instance" "main_instance" {
+  allocated_storage      = var.allocated_storage
+  storage_type           = var.storage_type
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  instance_class         = var.instance_class
+  db_name                = var.db_name
+  username               = var.username
+  password               = var.password
+  vpc_security_group_ids = var.vpc_security_group_ids
+  publicly_accessible    = var.publicly_accessible
+}
+
+
+
---
+++ rds_instance/variables.tf
@@ -0,0 +1,49 @@
+variable "db_name" {
+  type = string
+  default = "instance1"
+}
+
+variable "allocated_storage" {
+  type    = number
+  default = 20
+}
+
+variable "storage_type" {
+  type    = string
+  default = "gp2"
+}
+
+variable "engine" {
+  type    = string
+  default = "mysql"
+}
+
+variable "engine_version" {
+  type    = string
+  default = "5.7"
+}
+
+variable "instance_class" {
+  type    = string
+  default = "db.t3.micro"
+}
+
+variable "username" {
+  type = string
+  default = "admin"
+}
+
+variable "password" {
+  type = string
+  default = "password"
+}
+
+variable "vpc_security_group_ids" {
+    type = list(string)
+    default = [ "sg-0c6991ff990f64300" ]
+}
+
+variable "publicly_accessible" {
+  type    = bool
+  default = true
+}
