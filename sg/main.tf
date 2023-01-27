7f152886f33b13218a2cd5a0c94caeeda61df3cb
mhmdnaufal <lafuan@hotmail.com>
2023-01-27 16:36:17 +0700

    add sg

--- sg/main.tf
+++ sg/main.tf
@@ -44,3 +44,28 @@ ingress {
  }
 
 }
+
+resource "aws_security_group" "sg" {
+  name   = "ecs-sg"
+  description = "SG for ECS NGINX"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    self        = "false"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "http"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecs-sg"
+  }
+}
