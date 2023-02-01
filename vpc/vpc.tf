721a7102510925c4dd3599b076c071e44146919e
mhmdnaufal <lafuan@hotmail.com>
2023-02-01 14:45:55 +0700

    add

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "${var.region}"
+  region = var.region
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "${var.vpc_cidr}"
+  cidr_block = var.vpc_cidr
 
   tags = {
     Name = var.nama_vpc
