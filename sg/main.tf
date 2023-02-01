e98b15dffcf24fea328dce242b3aa3bd6e848499
mhmdnaufal <lafuan@hotmail.com>
2023-02-01 09:12:02 +0700

    add sg

--- sg/main.tf
+++ sg/main.tf
@@ -2,15 +2,17 @@ provider "aws" {
  region = "ap-southeast-3"
 }
 
-data "aws_vpc" "default" {
- default = true
-
+data "aws_vpc" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = ["Main VPC"]
+  }
 }
 
 resource "aws_security_group" "web_server_sg" {
  name        = "web-server-sg"
  description = "Allow HTTPS to web server"
- vpc_id      = data.aws_vpc.default.id
+ vpc_id      = data.aws_vpc.selected.id
 
 ingress {
    description = "HTTPS ingress"
@@ -33,7 +35,7 @@ egress {
 resource "aws_security_group" "ssh_access" {
  name        = "ssh_access"
  description = "Allow SSH for EC2"
- vpc_id      = data.aws_vpc.default.id
+ vpc_id      = data.aws_vpc.selected.id
 
 ingress {
    description = "SSH Access"
@@ -48,6 +50,7 @@ ingress {
 resource "aws_security_group" "sg" {
   name   = "ecs-sg"
   description = "SG for ECS NGINX"
+  vpc_id      = data.aws_vpc.selected.id
 
   ingress {
     from_port   = 80
